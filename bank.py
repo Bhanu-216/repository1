f3869e0c449c5e3e30c79a4fde036064b6565e61
PALUSAM HARI SHANKAR <245123735310@mvsrec.edu.in>
2025-10-03 12:27:28 +0530

    Add bank.py

---
+++ bank.py
@@ -0,0 +1,6 @@
+print("Bank of codédex")
+pin = int(input("Enter your pin : "))
+while pin != 1234 :
+	pin = int(input("Invalid pin !. Please enter a valid pin : "))
+if pin == 1234:
+	print("Correct pin")
